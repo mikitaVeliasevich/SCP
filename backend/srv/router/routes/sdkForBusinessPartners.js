f55f322ea4011e6ab721fdf38ae71d18a252c293
Nikita Velesevich <velesevich00@mail.ru>
2020-01-06 17:56:27 +0300

    error sdk

--- backend/srv/router/routes/sdkForBusinessPartners.js
+++ backend/srv/router/routes/sdkForBusinessPartners.js
@@ -6,32 +6,46 @@ const express = require("express");
 async function getAllBusinessPartners(){
     return BusinessPartner.requestBuilder()
         .getAll()
-        .select(
-            BusinessPartner.BUSINESS_PARTNER,
-            BusinessPartner.FIRST_NAME,
-            BusinessPartner.LAST_NAME
-        )
-        .filter(
-            BusinessPartner.BUSINESS_PARTNER_CATEGORY.equals('1')
-        )
+        // .select(
+        //     BusinessPartner.BUSINESS_PARTNER,
+        //     BusinessPartner.FIRST_NAME,
+        //     BusinessPartner.LAST_NAME
+        // )
+        // .filter(
+        //     BusinessPartner.BUSINESS_PARTNER_CATEGORY.equals('1')
+        // )
         .execute({
-            destinationName: "E01"
+            //destinationName: "S4C"
+            destinationConfiguration: {
+                test: "test",
+                url: "https://88.99.149.214:44300",
+                username: "CLOUD",
+                password: "309524be90f9e1270dc3cbad146d04d8",
+                usage: "pt_system",
+                isTrustingAllCertificates: true,
+                Name: "CentralPTSystem",
+                Description: "Central PT S4HANA",
+                URL: "http://centra.pt.system:44300"
+            }
         });
 }
 
 module.exports = () => {
     const app = express.Router();
 
-    app.get("/", async (req, res) =>{
+    app.get("/", async (req, res, next) => {
         getAllBusinessPartners()
             .then(businessPartners => {
                 res.status(200).send(businessPartners);
             })
             .catch(error => {
-                res.status(500).send(error.message);
-            });
+                res.status(500).send(error.stack);
+            })
     });
 
     return app;
 };
+
+
+
 
